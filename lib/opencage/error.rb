7de168d5ac1009e9dc198962f668145d36379dc1
Alessio Russo Introito <russointroitoa@gmail.com>
2022-11-01 15:15:11 +0100

    Raise errors (#16)
    Instead of raising generic `OpenCage::Geocoder::GeocodingError`, now raising `OpenCage::Error::<label>`

---
+++ lib/opencage/error.rb
@@ -0,0 +1,37 @@
+module OpenCage
+  class Error < StandardError
+    attr_reader :code
+
+    InvalidRequest = Class.new(self)
+    AuthenticationError = Class.new(self)
+    QuotaExceeded = Class.new(self)
+    Forbidden = Class.new(self)
+    InvalidEndpoint = Class.new(self)
+    NotAllowedMethod = Class.new(self)
+    Timeout = Class.new(self)
+    RequestTooLong = Class.new(self)
+    UpgradeRequired = Class.new(self)
+    TooManyRequests = Class.new(self)
+    InternalServer = Class.new(self)
+
+    def initialize(message:, code: nil)
+      super(message)
+      @code = code
+    end
+
+    ERRORS = {
+      0 => OpenCage::Error,
+      400 => OpenCage::Error::InvalidRequest,
+      401 => OpenCage::Error::AuthenticationError,
+      402 => OpenCage::Error::QuotaExceeded,
+      403 => OpenCage::Error::Forbidden,
+      404 => OpenCage::Error::InvalidEndpoint,
+      405 => OpenCage::Error::NotAllowedMethod,
+      408 => OpenCage::Error::Timeout,
+      410 => OpenCage::Error::RequestTooLong,
+      426 => OpenCage::Error::UpgradeRequired,
+      429 => OpenCage::Error::TooManyRequests,
+      503 => OpenCage::Error::InternalServer
+    }.freeze
+  end
+end
