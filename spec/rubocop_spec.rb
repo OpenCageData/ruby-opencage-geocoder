ffacb5e43df64ea7dff992c217dc787808ff8e38
marc tobias <mtmail@gmx.net>
2025-01-20 17:56:09 +0100

    README: we forgot to update classname of excetptions

--- spec/rubocop_spec.rb
+++ spec/rubocop_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path 'spec_helper.rb', __dir__
 describe 'Ruby linting', type: :feature do
   it 'run rubocop' do
     expect do
-      system('bundle exec rubocop --format simple 2>&1')
+      system('bundle exec rubocop --no-color --format simple 2>&1')
     end.to output(/no offenses detect/).to_stdout_from_any_process
   end
 end
